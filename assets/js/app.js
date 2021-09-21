3c9d5069c90a77f3c03f77a6eac58dd96819e4ab
hiddegrei <grei0005@hz.nl>
2021-09-21 11:36:48 +0200

    diamond code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,29 @@
-console.log('JavaScript is working!')
+let canvas;
+
+function setup(){
+     canvas=document.getElementById('canvas')
+
+}
+
+function drawDiamond(){
+    
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = 'rgb(200, 0, 0)';
+        ctx.beginPath();
+    ctx.moveTo(100, 100);
+    ctx.lineTo(150, 70);
+    ctx.lineTo(250, 70);
+    ctx.lineTo(300, 100);
+    ctx.lineTo(200, 300);
+    ctx.fill();
+      }
+
+}
+function draw(){
+    setup()
+    drawDiamond()
+}
+draw()
+
